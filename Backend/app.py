1ae70a7a6e6459d1d91c016ef0d2f6469400b20c
Edgar <210519@utxicotepec.edu.mx>
2024-06-17 18:18:05 -0600

    creación de archivos y carpetas

--- Backend/app.py
+++ Backend/app.py
@@ -1,1 +1,8 @@
+from fastapi import FastAPI
+from routes.user import user
+
+app = FastAPI()
+app.include_router(user)
+
+
 print("Hola bienvenido a mi backend")
---
+++ Backend/routes/user.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+
+user = APIRouter()
+
+@user.get("/users")
+
+def helloworld():
+    return "Hola 9B"
