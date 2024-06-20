26f54b89979d37cb2477e69f3043d18a11b3957a
Edgar <210519@utxicotepec.edu.mx>
2024-06-20 11:32:17 -0600

    Creación de un modelo de datos

--- Backend/routes/user.py
+++ Backend/routes/user.py
@@ -1,9 +1,33 @@
+#Importaciones
 from fastapi import APIRouter
-
+from pydantic import BaseModel
+from datetime import datetime
 
 user = APIRouter()
+users = []
+
+#userModel
+class model_user(BaseModel): #Creacion de un modelo de datos
+    id:str
+    usuario:str
+    password:str
+    created_at:datetime = datetime.now  #Formato de fecha y hora
+    estatus:bool=False
+    
+@user.get("/")
+
+def bienvenido():
+    return "Bienvenido al sistema de APIS"
+
 
 @user.get("/users")
 
-def helloworld():
-    return "Hola 9B"
+def get_usuarios():
+    return users        #Retornar todos los usuarios
+
+@user.post("/users")
+
+def save_usuarios(users:model_user):
+    #users.append
+    print (users)
+    return "Datos guardados"
