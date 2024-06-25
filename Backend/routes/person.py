3c9c0d3995309e07b228091a1bbc460adcafc624
Edgar <210519@utxicotepec.edu.mx>
2024-06-25 10:18:53 -0600

    creacion del archico person.py y de su insert, update, delete, search

---
+++ Backend/routes/person.py
@@ -0,0 +1,64 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+from datetime import datetime
+
+person = APIRouter()
+persons = [
+
+]
+class models_person(BaseModel):
+    id:str
+    nombre:str
+    primer_apellido: str
+    segundo_apellido: str
+    direccion: str
+    telefono:str
+    correo: str
+    sangre: str
+    fecha_nacimiento: datetime
+    created_at:datetime = datetime.now()
+    estatus:bool=False
+
+@person.get("/")
+
+def bienvenido():
+    return "Hola 9B"
+
+@person.get("/person", tags=['Personas'])
+
+def getPerson():
+    return persons
+
+@person.get("/person/{person_id}", tags=['Personas'])
+
+def getPerson(person_id: str):
+    for person in persons:
+        if person.id == person_id:
+            return person
+
+@person.post('/person', tags=['Personas'])
+
+def insertPerson(insert_person:models_person):
+    persons.append(insert_person)
+    return {"message": f"Se ha insertado un nuevo usuario con el ID: {insert_person.id}"}
+
+@person.put('/person/{person_id}', tags=['Personas'])
+
+def updatePerson(update_person:models_person, person_id: str):
+    print(update_person)
+    for index, person in enumerate(persons):
+        if person.id == person_id:
+            update_person.created_at = person.created_at
+        
+            persons[index] = update_person
+            
+            return {"message": f"Se ha modificado correctamente a la persona con el ID: {person_id}"}
+
+@person.delete('/person/{person_id}', tags=['Personas'])
+
+def deletePerson(person_id: str):
+    for index, person in enumerate(persons):
+        if person.id == person_id:
+            persons.pop(index)
+            return {"message": f"Se ha eliminado correctamente a la persona con el ID: {person_id}"}
+
--- Backend/routes/user.py
+++ Backend/routes/user.py
@@ -13,30 +13,30 @@ class models_user(BaseModel):
     created_at:datetime = datetime.now()
     estatus:bool=False
 
-@user.get("/")
+@user.get("/", tags=['Usuarios'])
 
 def bienvenido():
     return "Hola 9B"
 
-@user.get("/users")
+@user.get("/users", tags=['Usuarios'])
 
-def getUsers():
+def getUser():
     return users
 
-@user.get("/users/{user_id}")
+@user.get("/users/{user_id}", tags=['Usuarios'])
 
 def getUser(user_id: str):
     for user in users:
         if user.id == user_id:
             return user
 
-@user.post('/users')
+@user.post('/users', tags=['Usuarios'])
 
 def insertUser(insert_user:models_user):
     users.append(insert_user)
     return {"message": f"Se ha insertado un nuevo usuario con el ID: {insert_user.id}"}
 
-@user.put('/users/{user_id}')
+@user.put('/users/{user_id}', tags=['Usuarios'])
 
 def updateUser(update_user:models_user, user_id: str):
     print(update_user)
@@ -48,7 +48,7 @@ def updateUser(update_user:models_user, user_id: str):
             
             return {"message": f"Se ha modificado correctamente al usuario con el ID: {user_id}"}
 
-@user.delete('/users/{user_id}')
+@user.delete('/users/{user_id}', tags=['Usuarios'])
 
 def deleteUser(user_id: str):
     for index, user in enumerate(users):
