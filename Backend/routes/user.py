0628cca860ed3849ce493b4695b22d6b2552b156
Edgar <210519@utxicotepec.edu.mx>
2024-06-25 09:47:19 -0600

    Creación del delete, update y search de user

--- Backend/routes/user.py
+++ Backend/routes/user.py
@@ -1,33 +1,95 @@
-#Importaciones
 from fastapi import APIRouter
 from pydantic import BaseModel
 from datetime import datetime
 
 user = APIRouter()
-users = []
+users = [
 
-#userModel
-class model_user(BaseModel): #Creacion de un modelo de datos
+]
+class models_user(BaseModel):
     id:str
     usuario:str
-    password:str
-    created_at:datetime = datetime.now  #Formato de fecha y hora
+    contrasena: str
+    created_at:datetime = datetime.now()
     estatus:bool=False
-    
+
 @user.get("/")
 
 def bienvenido():
-    return "Bienvenido al sistema de APIS"
-
+    return "Hola 9B"
 
 @user.get("/users")
 
-def get_usuarios():
-    return users        #Retornar todos los usuarios
+def getUsers():
+    return users
 
-@user.post("/users")
+@user.get("/users/{user_id}")
 
-def save_usuarios(insert_users:model_user):
-    users.append(insert_users)
-    #print (insert_users)
-    return "Datos guardados"
+def getUser(user_id: str):
+    for user in users:
+        if user.id == user_id:
+            return user
+
+@user.post('/users')
+
+def insertUser(insert_user:models_user):
+    users.append(insert_user)
+    return {"message": f"Se ha insertado un nuevo usuario con el ID: {insert_user.id}"}
+
+@user.put('/users/{user_id}')
+
+def updateUser(update_user:models_user, user_id: str):
+    print(update_user)
+    for index, user in enumerate(users):
+        if user.id == user_id:
+            update_user.created_at = user.created_at
+        
+            users[index] = update_user
+            
+            return {"message": f"Se ha modificado correctamente al usuario con el ID: {user_id}"}
+
+@user.delete('/users/{user_id}')
+
+def deleteUser(user_id: str):
+    for index, user in enumerate(users):
+        if user.id == user_id:
+            users.pop(index)
+            return {"message": f"Se ha eliminado correctamente al usuario con el ID: {user_id}"}
+
+
+
+
+
+# #Importaciones
+# from fastapi import APIRouter
+# from pydantic import BaseModel
+# from datetime import datetime
+
+# user = APIRouter()
+# users = []
+
+# #userModel
+# class model_user(BaseModel): #Creacion de un modelo de datos
+#     id:str
+#     usuario:str
+#     password:str
+#     created_at:datetime = datetime.now  #Formato de fecha y hora
+#     estatus:bool=False
+    
+# @user.get("/")
+
+# def bienvenido():
+#     return "Bienvenido al sistema de APIS"
+
+
+# @user.get("/users")
+
+# def get_usuarios():
+#     return users        #Retornar todos los usuarios
+
+# @user.post("/users")
+
+# def save_usuarios(insert_users:model_user):
+#     users.append(insert_users)
+#     #print (insert_users)
+#     return "Datos guardados"
